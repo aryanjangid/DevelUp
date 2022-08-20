c69f1a84b45bd8287c37360e6b0af27759a84117
Aryan Jangid <aryanjangid2511@gmail.com>
2022-08-20 18:27:40 +0530

    search added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,16 @@ import { Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register.jsx';
-import Button from './components/Button';
+import Search from './pages/Search/Search';
 export default function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
-        
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/profile/:mail" element={<Login />} />
+        <Route exact path="/search" element={<Search />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
