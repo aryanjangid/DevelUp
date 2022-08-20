38167dc107aee75218ef45ff9a3d479d55cf067c
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-08-20 18:28:40 +0530

    profile page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,15 @@ import { Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register.jsx';
-import Button from './components/Button';
+import MentorProfile from './pages/MentorProfile/MentorProfile';
 export default function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
-        
         <Route exact path="/register" element={<Register />} />
-        <Route exact path="/profile/:mail" element={<Login />} />
+        <Route exact path="/profile/:mail" element={<MentorProfile />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
--- client/src/pages/MentorProfile/MentorProfile.jsx
+++ client/src/pages/MentorProfile/MentorProfile.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import classes from './mentorProfile.module.css'
+import { useParams } from "react-router";
+import { v4 as uuid } from "uuid";
 
 export default function MentorProfile() {
 
@@ -10,8 +12,88 @@ export default function MentorProfile() {
     const [github, setGithub] = useState("");
     const [linkedin, setLinkedin] = useState("");
 
-    const [editInfo, setEditInfo] = useState(0);
-    const [editBio, setEditBio] = useState(0);
+    const [bio, setBio] = useState("Enter your bio")
+    const [editInfo, setEditInfo] = useState(false);
+    const [editBio, setEditBio] = useState(false);
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [createRoom, setCreateRoom] = useState(false)
+    const [newRoomName, setNewRoomName] = useState('')
+
+    const [rooms, setRooms] = useState([])
+
+    let { mail } = useParams()
+
+    const navigate = useNavigate()
+
+    const getData = async () => {
+        const req = await fetch(`http://localhost:4000/mentor/${mail}`)
+        const mentor = await req.json()
+        if (mentor['mentor'][0]) {
+            setBio(mentor['mentor'][0].bio)
+            setFacebook(mentor['mentor'][0].facebook)
+            setInstagram(mentor['mentor'][0].instagram)
+            setTwitter(mentor['mentor'][0].twitter)
+            setGithub(mentor['mentor'][0].github)
+            setLinkedin(mentor['mentor'][0].linkedin)
+            setName(mentor['mentor'][0].name)
+            setEmail(mentor['mentor'][0].email)
+            setRooms(mentor['mentor'][0].rooms)
+        }
+        else {
+            navigate('/')
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const update = async (e) => {
+        e.preventDefault()
+        const req = await fetch(`http://localhost:4000/mentor/${mail}`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            body: JSON.stringify({
+                bio,
+                facebook,
+                instagram,
+                twitter,
+                github,
+                linkedin,
+            })
+        })
+        setEditInfo(false)
+        setEditBio(false)
+        const result = await req.json()
+    }
+
+    const createNewRoom = async (e) => {
+        setCreateRoom(false)
+        if (newRoomName === "") {
+            return
+        }
+        const newRoom = {
+            name: newRoomName,
+            roomId: uuid()
+        }
+        e.preventDefault()
+        const req = await fetch(`http://localhost:4000/mentor/${mail}`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            body: JSON.stringify({
+                rooms: [
+                    ...rooms, newRoom
+                ]
+            })
+        })
+        const result = await req.json()
+        console.log(result)
+    }
 
     return (
         <div className={classes.MainDiv}>
@@ -22,57 +104,57 @@ export default function MentorProfile() {
                 <div className={classes.profileRightdiv}>
                     <div className={classes.basicDetails}>
                         <h1 style={{ color: '#6225E6', fontSize: '1.4rem', marginBottom: '1rem' }}>Hey!</h1>
-                        <h1>Aryan Jangid</h1>
-                        <h3><span>Email: </span>aryanjangid@gmail.com</h3>
+                        <h1>{name}</h1>
+                        <h3><span>Email: </span>a{email}</h3>
                         <div>
                             <div style={{ marginTop: "2rem", marginBottom: "2rem" }} className={classes.wrapper}>
-                                <div className={classes.button}>
-                                    <a href={facebook} target="_blank">
+                                {facebook !== '' ? <a href={facebook} target="_blank" style={{ color: "#4267b2" }}>
+                                    <div className={classes.button}>
                                         <div className={classes.icon}>
                                             <i className="fab fa-facebook-f" />
                                         </div>
                                         <span>Facebook</span>
-                                    </a>
-                                </div>
-                                <a href={facebook} target="_blank">
+                                    </div>
+                                </a> : ''}
+                                {twitter !== '' ? <a href={twitter} target="_blank" style={{ color: "#1da1f2" }}>
                                     <div className={classes.button}>
                                         <div className={classes.icon}>
                                             <i className="fab fa-twitter" />
                                         </div>
                                         <span>Twitter</span>
-                                    </div></a>
-                                <a href={facebook} target="_blank">
+                                    </div></a> : ''}
+                                {instagram !== '' ? <a href={instagram} target="_blank" style={{ color: "#e1306c" }}>
                                     <div className={classes.button}>
                                         <div className={classes.icon}>
                                             <i className="fab fa-instagram" />
                                         </div>
                                         <span>Instagram</span>
-                                    </div></a>
-                                <a href={facebook} target="_blank">
-                                    <div className={classes.button}>
+                                    </div></a> : ''}
+                                {github !== '' ? <a href={github} target="_blank" style={{ color: "#333" }}>
+                                    <div className={classes.button} >
                                         <div className={classes.icon}>
                                             <i className="fab fa-github" />
                                         </div>
                                         <span>Github</span>
-                                    </div></a>
-                                <a href={facebook} target="_blank">
+                                    </div></a> : ''}
+                                {linkedin !== '' ? <a href={linkedin} target="_blank" style={{ color: "#0077b5" }}>
                                     <div className={classes.button}>
                                         <div className={classes.icon}>
                                             <i className="fab fa-linkedin" />
                                         </div>
                                         <span>Linkedin</span>
-                                    </div></a>
+                                    </div></a> : ''}
                             </div>
-                            <div onClick={() => setEditInfo(1 - editInfo)} className={classes.editButton}><h1>Edit Links</h1></div>
+                            <div onClick={() => setEditInfo(true)} className={classes.editButton}><h1>Edit Links</h1></div>
                         </div>
-                        {editInfo == 1 ?
+                        {editInfo ?
                             <div className={classes.editContainer}>
-                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='Facebook'></input>
-                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='Twitter'></input>
-                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='Instagram'></input>
-                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='GitHub'></input>
-                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='Linkedin'></input>
-                                <div style={{ backgroundColor: 'green' }} className={classes.editButton}><h1>Save</h1></div>
+                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='Facebook' onChange={(e) => setFacebook(e.target.value)} value={facebook} ></input>
+                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='Twitter' onChange={(e) => setTwitter(e.target.value)} value={twitter} ></input>
+                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='Instagram' onChange={(e) => setInstagram(e.target.value)} value={instagram} ></input>
+                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='GitHub' onChange={(e) => setGithub(e.target.value)} value={github} ></input>
+                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='Linkedin' onChange={(e) => setLinkedin(e.target.value)} value={linkedin}></input>
+                                <div style={{ backgroundColor: 'green' }} className={classes.editButton} onClick={update}><h1>Save</h1></div>
                             </div>
                             :
                             <></>
@@ -81,26 +163,33 @@ export default function MentorProfile() {
                     </div>
                     <div className={classes.basicDetails}>
                         <h1>Bio</h1>
-                        <h3 style={{margin:'2rem', marginLeft: '0'}}>Currently I am a sophomore pursuing Artificial Intelligence and Data Science Engineering. Building a competitive programming community. Skilled in C++, Java, Designing, Website Building, Engineering, and Front-end Development. Love to solve hard problems and building fast and scalable applications. I am also interested in Virtual Reality (VR) technologies and building that as a side project.</h3>
-                        <div onClick={() => setEditBio(1 - editBio)} className={classes.editButton}><h1>Edit Bio</h1></div>
-                        {editBio==1?
-                        <div>
-                            <textarea className={classes.textArea}></textarea>
-                            <div style={{ backgroundColor: 'green' , marginTop:"2rem"}} className={classes.editButton}><h1>Save</h1></div>
-                        </div>
-                        :
-                        <></>
-                    }
+                        <h3 style={{ margin: '2rem', marginLeft: '0' }}>{bio}</h3>
+                        <div onClick={() => setEditBio(true)} className={classes.editButton}><h1>Edit Bio</h1></div>
+                        {editBio ?
+                            <div>
+                                <textarea className={classes.textArea}></textarea>
+                                <div style={{ backgroundColor: 'green', marginTop: "2rem" }} className={classes.editButton} onClick={update}><h1>Save</h1></div>
+                            </div>
+                            :
+                            <></>
+                        }
                     </div>
                     <div className={classes.basicDetails}>
                         <h1>Rooms</h1>
-                        <div className={classes.roomDiv} style={{ marginLeft: "0" }}><h3>Room 1</h3>  <div className={classes.viewRoomButton}>View</div></div>
+                        {rooms.map((room, index) => {
+                            return <div className={classes.roomDiv} style={{ marginLeft: "0" }} key={index}><h3>{room.name}</h3><div className={classes.viewRoomButton}>View</div></div>
+                        })}
+                        {createRoom === false ? <div style={{ backgroundColor: 'green', marginTop: "2rem" }} className={classes.editButton} onClick={() => setCreateRoom(true)}><h1>Create room</h1></div> :
+                            (<>
+                                <input style={{ width: '80%', marginLeft: '0' }} className="inputBox" placeholder='Room Name' onChange={(e) => setNewRoomName(e.target.value)} value={newRoomName} ></input>
+                                <div style={{ backgroundColor: 'green', marginTop: "2rem" }} className={classes.editButton} onClick={createNewRoom}><h1>Save room</h1></div>
+                            </>)}
                     </div>
 
                 </div>
 
             </div>
 
-        </div>
+        </div >
     )
 }
