c737e3acd98aef2969ee92a3603997fcc714c43b
Aryan Jangid <aryanjangid2511@gmail.com>
2022-08-21 02:28:21 +0530

    three js added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,19 +9,18 @@ import Search from './pages/Search/Search'
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Room from "./pages/Room/Room";
-// import { Model } from "./components/Models/Blender";
-
+import StudyGroup from "./pages/Study Group/StudyGroup";
 export default function App() {
-  // const [action1, setAction1] = useState("aryan");
+  const [action1, setAction1] = useState("aryan");
 
   return (
     <div>
-      
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/search" element={<Search />} />
+        <Route exact path="/studygroups" element={<StudyGroup />} />
         <Route exact path="/profile/:mail" element={<MentorProfile />} />
         <Route exact path="/room" element={<Room />} />
         <Route path="*" element={<Navigate to="/" />} />
