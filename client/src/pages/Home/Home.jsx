c7578150bc38b1a4b2698d705982e75178aa99dc
Aryan Jangid <aryanjangid2511@gmail.com>
2022-08-21 03:32:17 +0530

    Study Group added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { Suspense } from 'react';
 import Button from '../../components/Button'
+
+import Model from '../../components/Models/Team';
+
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 export default function Home() {
   return (
     <div>
-      <h1 style={{color:"black"}}>Home</h1>
+      <h1 style={{ color: "black" }}>Home</h1>
       <Button name="login" to="login"></Button>
       <Button name="mentor-profile" to="mentor-profile"></Button>
+      <Canvas style={{height: "30rem"}}>
+        <OrbitControls enableDamping={true} enableZoom={true} />
+
+        <directionalLight position={[4, 5, 2]} intensity={1} color="#F32053" />
+        <directionalLight position={[-4, -5, -2]} intensity={1} color="#A8ECF0" />
+        <directionalLight position={[4, -5, 2]} intensity={1} color="#407BFF" />
+
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+      </Canvas>
     </div>
   )
 }
--- client/src/pages/Study Group/StudyGroup.jsx
+++ client/src/pages/Study Group/StudyGroup.jsx
@@ -1,19 +1,80 @@
 import React from 'react'
 import classes from './studyGroup.module.css'
 import background from '../../Assets/studyBackground.png'
+import profile1 from '../../Assets/profile1.png'
+import profile2 from '../../Assets/profile2.png'
+import profile3 from '../../Assets/profile3.png'
+import profile4 from '../../Assets/profile4.png'
+import profile5 from '../../Assets/profile5.png'
+import profile6 from '../../Assets/profile6.png'
+import profile7 from '../../Assets/profile7.png'
+import profile8 from '../../Assets/profile8.png'
 
 export default function StudyGroup() {
+
+  const profilePhotos = [profile1, profile2, profile3, profile4, profile5, profile6, profile7, profile8];
+  const giveRandom = () => {
+    return Math.floor(Math.random() * 7);
+  }
   return (
     <div style={{
-      backgroundImage: `url(${background})`, height: 'fitContent',padding:'2rem'
+      backgroundImage: `url(${background})`, height: 'fitContent', padding: '2rem'
     }}>
       <h1 style={{ textAlign: 'center', padding: "2rem" }}>Study Groups</h1>
       <div className={classes.studyGroupDiv}>
-          <h1> Search Your favorite group here</h1>
-            <div className={classes.searchDiv}>
-              <input style={{width:'70%'}} className='inputBox' placeholder='Codery...'></input>
-              <div></div>
+        <h1> Search Your favorite group here</h1>
+        <div className={classes.searchDiv}>
+          <input style={{ width: '70%' }} className='inputBox' placeholder='Codery...'></input>
+          <div> Search</div>
+        </div>
+        <div className={classes.allGroups}>
+          <div className={classes.studyGroup}>
+            <div className={classes.groupDiv}>
+              <div className={classes.studyGroupDetails}>
+                <img className={classes.studyGroupImg} src={profilePhotos[giveRandom()]} alt="profile"></img>
+                <h3 style={{ marginLeft: '1rem', fontSize: '1.4rem' }}>Competetive Programming</h3>
+              </div>
+              <div className={classes.viewButton}>
+                View Group
+              </div>
             </div>
+            <h3 style={{ paddingTop: '2rem', textAlign: 'left' }}>Learning and refining competitive programming to crack interviews better and improve critical thinking.</h3>
+          </div>
+          <div className={classes.studyGroup}>
+            <div className={classes.groupDiv}>
+              <div className={classes.studyGroupDetails}>
+                <img className={classes.studyGroupImg} src={profilePhotos[giveRandom()]} alt="profile"></img>
+                <h3 style={{ marginLeft: '1rem', fontSize: '1.4rem' }}>Competetive Programming</h3>
+              </div>
+              <div className={classes.viewButton}>
+                View Group
+              </div>
+            </div>
+            <h3 style={{ paddingTop: '2rem', textAlign: 'left' }}>Learning and refining competitive programming to crack interviews better and improve critical thinking.</h3>
+          </div><div className={classes.studyGroup}>
+            <div className={classes.groupDiv}>
+              <div className={classes.studyGroupDetails}>
+                <img className={classes.studyGroupImg} src={profilePhotos[giveRandom()]} alt="profile"></img>
+                <h3 style={{ marginLeft: '1rem', fontSize: '1.4rem' }}>Competetive Programming</h3>
+              </div>
+              <div className={classes.viewButton}>
+                View Group
+              </div>
+            </div>
+            <h3 style={{ paddingTop: '2rem', textAlign: 'left' }}>Learning and refining competitive programming to crack interviews better and improve critical thinking.</h3>
+          </div><div className={classes.studyGroup}>
+            <div className={classes.groupDiv}>
+              <div className={classes.studyGroupDetails}>
+                <img className={classes.studyGroupImg} src={profilePhotos[giveRandom()]} alt="profile"></img>
+                <h3 style={{ marginLeft: '1rem', fontSize: '1.4rem' }}>Competetive Programming</h3>
+              </div>
+              <div className={classes.viewButton}>
+                View Group
+              </div>
+            </div>
+            <h3 style={{ paddingTop: '2rem', textAlign: 'left' }}>Learning and refining competitive programming to crack interviews better and improve critical thinking.</h3>
+          </div>
+        </div>
       </div>
     </div>
   )
