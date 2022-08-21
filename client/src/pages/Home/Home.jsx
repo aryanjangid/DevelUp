a42228964b7e36407599889a205333ecfb2c55f3
Aryan Jangid <aryanjangid2511@gmail.com>
2022-08-21 07:35:04 +0530

    home page done

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Button from '../../components/Button'
 import classes from './home.module.css'
 import Model from '../../components/Models/Team';
 import background from '../../Assets/chatsbackground.png'
+import tempback from '../../Assets/tempback.png'
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
@@ -16,7 +17,7 @@ export default function Home() {
         <div className={classes.landingLeftDiv}>
           <h1><span>MENTORING AND HELPING</span> EACH OTHER TO LEARN NEW SKILLS</h1>
           <h2 >We are building a robust and influential network that will assist juniors in connecting with and learning from their mentors </h2>
-          <div style={{marginTop:"3rem"}}><Button center="1" name="Explore" to="/search"></Button></div>
+          <div style={{ marginTop: "3rem" }}><Button color="#a351ae" center="1" name="Explore" to="/search"></Button></div>
         </div>
         <div className={classes.threejsDiv}>
           <Canvas>
@@ -33,6 +34,20 @@ export default function Home() {
         </div>
       </div>
 
+
+      <div className={classes.secondDiv} style={{
+      backgroundImage: `url(${tempback})`, height: "fitContent"}}>
+        <div className={classes.nextDivs}>
+          <h1>Want to <span>learn new skill</span></h1>
+          <h3>Our secondary goal is to build a strong mentoring relationship between our seasoned seniors and juniors</h3>
+          <Button center="1" name="Search Here" to="/search" color="#4262ff"></Button>
+        </div>
+        <div className={classes.nextDivs}>
+          <h1>Check out our <span style={{backgroundColor: "#a259ff"}}>Study Groups</span></h1>
+          <h3>This will allow the students to learn a lot in a short amount of time while also allowing them to form strong bonds with their peers.</h3>
+          <Button center="1" name="Check Now" to="/search" color="#4262ff"></Button>
+        </div>
+      </div>
 
     </div>
   )
